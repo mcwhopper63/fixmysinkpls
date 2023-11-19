cfcaf9f7dc0ee8ef4f6887faa5e5ccfd45c603f7
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-11-19 09:53:26 -0500

    setting up server and testing out thunder client

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('server is working');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World get method');
+});
+
+app.post('/', (req, res) => {
+    console.log(req);
+
+    res.json({ message: 'Data Received post method', data: req.body });
+});
+
+app.listen(5100, () => {
+    console.log('server is listening at port 5100');
+});
