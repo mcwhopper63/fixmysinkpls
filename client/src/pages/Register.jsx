46b4410138576f3130f0774a3227f0a76950f916
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-12-24 08:00:22 -0500

    custom fetch method to post new users

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,16 @@
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { Logo, FormRow } from '../components';
+import customFetch from '../utils/customFetch';
 
 export const action = async ({ request }) => {
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
-    console.log(data);
     try {
         await customFetch.post('/auth/register', data);
         return redirect('/login');
     } catch (error) {
+        console.log(error);
         return error;
     }
 };
@@ -23,15 +24,15 @@ const Register = () => {
                 <FormRow type="text" name="name" defaultValue="John" />
                 <FormRow
                     type="text"
-                    name="last name"
+                    name="lastName"
                     labelText="Last Name"
                     defaultValue="Tenant"
                 />
                 <FormRow
                     type="text"
-                    name="propertyLocation"
+                    name="location"
                     labelText="Property Location"
-                    defaultValue="Sleepy Hills"
+                    defaultValue="202"
                 />
                 <FormRow
                     type="text"
