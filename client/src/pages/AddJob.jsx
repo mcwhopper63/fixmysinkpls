02e0a16f96db48f4c96a292cc503c13fb339548b
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2023-12-24 17:02:37 -0500

    building out add job page

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,10 +1,39 @@
-import { Logo } from '../components';
+import { FormRow } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useOutletContext } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
 
 const AddJob = () => {
+    const { user } = useOutletContext();
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+
     return (
-        <div>
-            <h1>add job</h1>
-        </div>
+        <Wrapper>
+            <Form method="post" className="form">
+                <h4 className="form-title">add job</h4>
+                <div className="form-center">
+                    <FormRow type="text" name="position" />
+                    <FormRow type="text" name="company" />
+                    <FormRow
+                        type="text"
+                        name="jobLocation"
+                        labelText="job location"
+                        defaultValue={user.location}
+                    />
+                    <button
+                        className="btn btn-block form-btn"
+                        type="submit"
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? 'submitting...' : 'submit'}
+                    </button>
+                </div>
+            </Form>
+        </Wrapper>
     );
 };
 export default AddJob;
