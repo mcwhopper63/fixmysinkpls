487ba5fe89cff3a945fc0f28ee5a4eb84dc81607
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-01-04 17:13:53 -0500

    done?

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -7,13 +7,26 @@ const PageBtnContainer = () => {
     const {
         data: { numOfPages, currentPage },
     } = useAllJobsContext();
-    console.log(numOfPages, currentPage);
-
+    const { search, pathname } = useLocation();
+    const navigate = useNavigate();
     const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
+
+    const handlePageChange = (pageNumber) => {
+        const searchParams = new URLSearchParams(search);
+        searchParams.set('page', pageNumber);
+        navigate(`${pathname}?${searchParams.toString()}`);
+    };
 
     return (
         <Wrapper>
-            <button className="btn prev-btn">
+            <button
+                className="btn prev-btn"
+                onClick={() => {
+                    let prevPage = currentPage - 1;
+                    if (prevPage < 1) prevPage = numOfPages;
+                    handlePageChange(prevPage);
+                }}
+            >
                 <HiChevronDoubleLeft />
                 prev
             </button>
@@ -24,16 +37,25 @@ const PageBtnContainer = () => {
                             pageNumber === currentPage && 'active'
                         }`}
                         key={pageNumber}
+                        onClick={() => handlePageChange(pageNumber)}
                     >
                         {pageNumber}
                     </button>
                 ))}
             </div>
-            <button className="btn next-btn">
+            <button
+                className="btn next-btn"
+                onClick={() => {
+                    let nextPage = currentPage + 1;
+                    if (nextPage > numOfPages) nextPage = 1;
+                    handlePageChange(nextPage);
+                }}
+            >
                 next
                 <HiChevronDoubleRight />
             </button>
         </Wrapper>
     );
 };
+
 export default PageBtnContainer;
