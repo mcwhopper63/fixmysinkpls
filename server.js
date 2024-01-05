b8b02da9353807ce3eda783fed2df3a0cfd66eb9
Taeho Lee <31860067+wheels63@users.noreply.github.com>
2024-01-05 10:24:39 -0500

    deployment success

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import 'express-async-errors';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
@@ -22,12 +23,6 @@ import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
 // import { validateJobInput } from './middleware/validationMiddleware.js';
 
-const app = express();
-
-if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan('dev'));
-}
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
@@ -36,7 +31,14 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, '.client/public')));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+// }
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 // middleware to use cookie parser
 app.use(cookieParser());
@@ -56,25 +58,24 @@ app.get('/api/v1/test', (req, res) => {
 
 // middleware for creating, editing,  a job, deleting a job, and
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
-
+// middleware for creating, editing,  a job, deleting a user
+app.use('/api/v1/users', authenticateUser, userRouter);
 // middleware for creating, editing,  a job, deleting a user
 app.use('/api/v1/auth', authRouter);
 
-// middleware for creating, editing,  a job, deleting a user
-app.use('/api/v1/users', authenticateUser, userRouter);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+});
 
 // Not Found Middleware
 app.use('*', (req, res) => {
     res.status(404).json({ msg: 'not found' });
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
-});
 // Error Middleware
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 5100;
 
 try {
     await mongoose.connect(process.env.MONGO_URL);
